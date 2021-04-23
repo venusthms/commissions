ae909c30f0a04ca9b9a7d3292c0d3607246e6d6b
Sabrina <48449995+venusthms@users.noreply.github.com>
2021-04-23 17:36:30 +0200

    Create header.js

---
+++ twelve/header.js
@@ -0,0 +1,8 @@
+var elementPosition = $('header').offset();
+$(window).scroll(function() {
+   if ($(window).scrollTop() > elementPosition.top) {
+      $('header').addClass('fixed');
+   } else {
+      $('header').removeClass('fixed');
+   }
+});
